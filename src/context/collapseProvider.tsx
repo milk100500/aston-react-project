971066df8fd338b32fe85bb204973d4493a31f19
milk100500 <LedVanLed@yandex.ru>
2023-12-02 18:18:19 +0300

    Добавление useMemo в CollapseProvider

--- src/context/collapseProvider.tsx
+++ src/context/collapseProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState, useMemo } from "react";
 
 interface CollapseContextInterface {
   collapsed: boolean;
@@ -14,13 +14,10 @@ const CollapseContextProvider = ({ children }: { children: React.ReactNode }) =>
     const [collapsed, setCollapsed] = useState(true);
     const switchCollapse = () => setCollapsed((prev) => !prev);
 
+    const value = useMemo(() => ({ collapsed, switchCollapse }), [collapsed]);
+
     return (
-        <CollapseContext.Provider
-            value={{
-                collapsed,
-                switchCollapse,
-            }}
-        >
+        <CollapseContext.Provider value={value}>
             {children}
         </CollapseContext.Provider>
     );
